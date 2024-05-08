b5a93aba9ed6255d552d8c1d1c31489586643835
Yevhen Lebid <yevhenlebid51@gmail.com>
2024-05-08 23:05:26 +0300

    Implement modal and share functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     let scroll = window.scrollY || document.documentElement.scrollTop;
     let header = document.querySelector('.header-js');
 
@@ -14,3 +14,43 @@ window.addEventListener("scroll", function() {
         }
     }
 });
+
+function modalInit() {
+    // Get the body
+    let body = document.querySelector('body')
+
+    // Get the modal
+    let modal = document.getElementById('share-modal');
+
+    // Get the button that opens the modal
+    let shareButtons = document.querySelectorAll('.share-modal-open-js');
+
+    // Get the <span> element that closes the modal
+    let modalClose = modal.querySelector('.modal__close-js');
+
+    if (modal && shareButtons && modalClose) {
+        // When the user clicks on the button, open the modal
+        shareButtons.forEach((btn) => {
+            btn.addEventListener('click', function (e) {
+                modal.classList.add('modal__open');
+                body.classList.add('modal-open');
+            })
+        })
+
+        // When the user clicks on <span> (x), close the modal
+        modalClose.onclick = function () {
+            modal.classList.remove('modal__open');
+            body.classList.remove('modal-open');
+        }
+
+        // When the user clicks anywhere outside the modal, close it
+        window.onclick = function (event) {
+            if (event.target === modal) {
+                modal.classList.remove('modal__open');
+                body.classList.remove('modal-open');
+            }
+        }
+    }
+}
+
+modalInit();
